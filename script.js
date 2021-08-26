bec9512da3c17b3c2b6022613ffc338e1d505217
Alexan345 <87387229+Alexan345@users.noreply.github.com>
2021-08-26 11:36:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+/*let str = 'abcde';
+console.log(str[0]);
+console.log(str[1]);
+console.log(str[4]);
+
+let a = 60;
+let b = 60;
+console.log(a*b);
+
+let num = 3;
+console.log(num);*/
+    
+    
+/*let a = 10;
+let b = 2;
+console.log(a - b);
+console.log(a * b);
+console.log(a % b);*/
+
+/*let c = 15 ;
+let d = 2 ;
+let result =(c + d);
+console.log( result);*/
+
+/*let a = 10;
+let b = 2;
+let c = 5;
+console.log(a + b + c);*/
+
+/*let a = 17;
+let b = 10;
+let c = (a - b);
+let d = 7;
+let result = (c + d);
+console.log(result);*/
+
+
+/*let str = 'Hello, World!';
+console.log(str);*/
+
+
+/*let str1 = 'Hello, ';
+let str2 = 'World!';
+console.log(str1 + str2);*/
+
+/*let name = 'Alex!';
+let z = 'Salut, ';
+console.log(z + name);*/
+
+
+/*let age = '25 ';
+let e = 'Eu am ';
+let r = 'ani!';
+console.log(e + age + r);*/
+
+
+/*let str = 'aaa bbb ccc';
+console.log(str.substring(4,7)); 
+console.log(str.slice(4,7));
+console.log(str.substr(4, 3));*/
+
+
+ 
+/*let str = 'js';
+console.log(str.toUpperCase());*/
+
+/*let str = 'JS';
+console.log(str.toLowerCase());*/
+
+/*let str = 'i learn javascript';
+console.log(str.substring(2,7) + str.substring(8,18));
+console.log(str.substr(2,5) + str.substr(8,10));
+console.log(str.slice(2,7) + str.slice(8,18));
+console.log(str.indexOf('learn'));*/
+
+/*let a  = 'i learn javascript!';
+console.log(a.length);*/
+
+
+
+
+
+/*let userData = {
+  firstname: 'Dave',
+  lastname: 'Doe',
+  age: 46,
+  statusemployee:true 
+
+}
+
+
+let divData = document.querySelector('#data');
+divData.innerHTML = `<p>firstname: ${userData.firstname}</p>
+<p>lastname: ${userData.lastname}</p>
+<p>age: ${userData.age}</p>
+<p>statusemployee: ${userData.statusemployee}</p>`;*/
+
+          
+
+
+      
